c1f1910a086de9acfc32e90574eb140e4bef2d6d
minialonez <minialonez@gmail.com>
2026-05-14 22:57:42 +0700

    Delete css directory

--- css/config.js
+++
@@ -1,8 +0,0 @@
-window.CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzarzLBCNsC9CLRNkwcdUo1RZZCrHpkkswhzNQWNuKJxqQZ28XIBHdd-JEWwkor1pdy/exec',
-  API_KEY: 'ALPHA BADMINTON-KRABI',
-  BUSINESS_NAME: 'ALPHA BADMINTON KRABI',
-  FACE_MATCH_THRESHOLD: 0.5,
-  FACE_DESCRIPTOR_SAMPLES: 5,
-  MODELS_URL: 'https://cdn.jsdelivr.net/gh/justadudewhohacks/face-api.js@master/weights',
-};
